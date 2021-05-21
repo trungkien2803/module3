ca0f9e84fcbdbae369c66a1764431a72704f8fab
Tran Trung Kien <tieulong2803200@gmail.com>
2021-05-21 14:00:51 +0700

    2

--- CreateTable/src/com/company/ClassicModels.sql
+++ CreateTable/src/com/company/ClassicModels.sql
@@ -17,3 +17,40 @@ begin
 end //
 
 call findAllCustomers();
+
+delimiter //
+create procedure getCusById(in cusNum int(11))
+begin
+    select * from customers where customerNumber = cusNum;
+end //
+delimiter ;
+
+call getCusById(175);
+
+delimiter //
+create procedure getCustomersCountbyCity(in in_city varchar(50), out total int)
+begin
+    select count(customerNumber)
+    into total
+    from customers
+    where city = in_city;
+end //
+delimiter ;
+
+call getCustomersCountbyCity('Lyon', @total);
+select @total;
+
+DELIMITER //
+CREATE PROCEDURE SetCounter(INOUT counter INT, IN inc INT)
+BEGIN
+    SET counter = counter + inc;
+END//
+DELIMITER ;
+
+set @counter = 1;
+
+call SetCounter(@counter,1);
+call SetCounter(@counter,1);
+call SetCounter(@counter,5);
+select @counter;
+
